2b8912d3b130d54047c3f449ce365d377ba5f510
Shuvkant <weird426344@gmail.com>
2023-10-16 22:13:09 +0545

    adding QuizApp

---
+++ JavascriptProjects/QuizApp/script.js
@@ -0,0 +1,122 @@
+const questions = [
+  {
+    question: "Which is largest animal in the world?",
+    answers: [
+      { text: "shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "Which country is known as the Land of the Rising Sun?",
+    answers: [
+      { text: "China", correct: false },
+      { text: "India", correct: false },
+      { text: "Japan", correct: true },
+      { text: "South Korea", correct: false },
+    ],
+  },
+  {
+    question: "Which is the national animal of Scotland?",
+    answers: [
+      { text: "Unicorn", correct: true },
+      { text: "Loch Ness Monster", correct: false },
+      { text: "Red Squirrel", correct: false },
+      { text: "Highland Cow", correct: false },
+    ],
+  },
+  {
+    question: "Which planet in our solar system has the most moons?",
+    answers: [
+      { text: "Earth", correct: false },
+      { text: "Jupiter", correct: true },
+      { text: "Saturn", correct: false },
+      { text: "Uranus", correct: false },
+    ],
+  },
+  {
+    question: "Which is the world's smallest ocean?",
+    answers: [
+      { text: "Indian Ocean", correct: false },
+      { text: "Atlantic Ocean", correct: false },
+      { text: "Arctic Ocean", correct: false },
+      { text: "Southern Ocean", correct: true },
+    ],
+  },
+];
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+startQuiz();
